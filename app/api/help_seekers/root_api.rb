910e805cfefe939cc79eafdf6943462829a0a6a4
Petru G. Cioată <petru.cioata@gmail.com>
2020-10-27 00:55:24 +0200

    Add root api for HelpSeekers & Volunteers

---
+++ app/api/help_seekers/root_api.rb
@@ -0,0 +1,31 @@
+module HelpSeekers
+  class RootApi < Grape::API
+    format :json
+
+    # before { authenticate_client_app! }
+
+    rescue_from Grape::Exceptions::ValidationErrors do |e|
+      error!('Bad request', 400)
+    end
+
+    rescue_from ActiveRecord::RecordNotFound do
+      error!('Not Found', 404)
+    end
+
+    http_basic do |username, password|
+      ENV['SWAGGER_USERNAME'] == username && ENV['SWAGGER_PASSWORD'] == password
+    end
+
+    # mount ProfilesApi
+
+    add_swagger_documentation(
+      format: :json,
+      base_path: '/help_seekers/api/v1',
+      mount_path: 'docs',
+      info: { title: 'HelpSeekers API docs' },
+      models: [],
+      array_use_braces: true,
+      add_root: true
+    )
+  end
+end
