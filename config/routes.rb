6e0d67b34af79b342485c24824aba19ba330cd93
biancabordianu <biancabordianu97@gmail.com>
2020-10-27 13:52:09 +0200

    add routes for signin and signup

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@
 
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope '/auth' do
+    post '/signin', to: 'user_token#create'
+    post '/signup', to: 'users#create'
+  end
   mount Volunteers::RootApi => '/volunteers/api/v1'
   mount HelpSeekers::RootApi => '/help_seekers/api/v1'
 end
