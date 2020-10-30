520b98f4b832f08ccf265bef10842d7ca9393908
Petru G. Cioată <petru.cioata@gmail.com>
2020-10-30 12:16:43 +0200

    Remove comment

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,7 +7,6 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-  # enum role: %i[help_seeker volunteer ngo admin]
   enum role: { help_seeker: 0, volunteer: 1, ngo: 2, admin: 3 }
 
   def self.from_token_request(request)
