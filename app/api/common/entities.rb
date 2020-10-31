5d77e0b09a4ce1390181469d82cb0f6a7572d2d9
Petru G. Cioată <petru.cioata@gmail.com>
2020-10-31 20:26:14 +0200

    Update entities structure

--- app/api/common/entities.rb
+++ app/api/common/entities.rb
@@ -4,9 +4,6 @@ module Common
       expose :phone_number
       expose :first_name
       expose :last_name
-      expose :email
-      expose :cif
-      expose :role
     end
 
     class Need < Grape::Entity
@@ -15,10 +12,6 @@ module Common
       expose :id, documentation: { type: Integer }
       expose :description
       expose :state
-      expose :contact_info
-      expose :contact_phone_number
-      expose :added_by, using: User
-      expose :chosen_by, using: User, expose_nil: true
     end
   end
 end
--- app/api/help_seekers/entities.rb
+++ app/api/help_seekers/entities.rb
@@ -1,6 +1,10 @@
 module HelpSeekers
   module Entities
+    class Volunteer < Common::Entities::User
+    end
+
     class Need < Common::Entities::Need
+      expose :chosen_by, using: Volunteer, expose_nil: true
     end
   end
 end
