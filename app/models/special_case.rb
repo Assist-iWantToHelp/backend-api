58dba7b6e26da7728095115a3644ae07001208ba
biancabordianu <biancabordianu97@gmail.com>
2020-11-02 13:09:11 +0200

    add alias for user

--- app/models/special_case.rb
+++ app/models/special_case.rb
@@ -2,5 +2,7 @@ class SpecialCase < ApplicationRecord
   belongs_to :user
   has_many :user_special_cases
 
+  alias added_by user
+
   validates :description, presence: true
 end
--- app/models/user_special_case.rb
+++ app/models/user_special_case.rb
@@ -2,5 +2,7 @@ class UserSpecialCase < ApplicationRecord
   belongs_to :special_case
   belongs_to :user
 
+  alias sustained_by user
+
   validates :promotion_description, presence: true
 end
