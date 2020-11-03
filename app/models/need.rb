f27c2ca440160b18fb2567433fa05e7f4ceb9867
Petru G. Cioată <petru.cioata@gmail.com>
2020-11-03 13:56:27 +0200

    Add recommended needs CRUD and some validations

--- app/models/need.rb
+++ app/models/need.rb
@@ -1,5 +1,5 @@
 class Need < ApplicationRecord
-  enum status: { opened: 0, in_progres: 1, completed: 2, closed: 3 }
+  enum status: { opened: 0, in_progress: 1, completed: 2, closed: 3 }
 
   belongs_to :added_by, class_name: 'User'
   belongs_to :chosen_by, class_name: 'User', optional: true
