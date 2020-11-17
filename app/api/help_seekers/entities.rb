619e53d383d7abc74ec8997942164e71163bc83a
AnamariaaNistor <anamaria.nistor@assist.ro>
2020-11-17 13:10:33 +0200

    Exposed status_updated_at and updated_by for needs

--- app/api/help_seekers/entities.rb
+++ app/api/help_seekers/entities.rb
@@ -6,6 +6,8 @@ module HelpSeekers
     class Need < Common::Entities::Need
       expose :chosen_by, using: Volunteer, expose_nil: true
       expose :reviews, using: Common::Entities::Review
+      expose :status_updated_at
+      expose :updated_by
     end
   end
 end
