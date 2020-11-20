15c9a5a07d207ac7cf152dcb0a2d509b33f60736
Petru G. Cioată <petru.cioata@gmail.com>
2020-11-20 20:42:28 +0200

    Expoose user among jwt token

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,6 +1,20 @@
 class UserTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token, raise: false
 
+  def create
+    user = User.find_by(id: auth_token.payload[:sub])
+
+    response = {
+      jwt: auth_token.token,
+      user: user.as_json(
+        include: :address,
+        except: %i[created_at updated_at password_digest]
+      )
+    }
+
+    render json: response, status: :created
+  end
+
   private
 
   def auth_params
